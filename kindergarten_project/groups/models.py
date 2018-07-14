4cabdc9061aef950976bbe99af381c636ac995c2
pakam2 <pkami.87@gmail.com>
2018-07-14 20:21:50 +0200

    Added a new app

---
+++ kindergarten_project/groups/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=255)
+    type_of_group = models.CharField(max_length=1, choices=GROUP)
+	child = models.ForeignKey(Child)
+    teachers = models.ManyToManyField(Teacher, related_name='teacher_of_group') // trzeba zaimportować z accounts
+
+    def __str__(self):
+      return self.group_name
+
+class Child(models.Model):
+    name = models.CharField(max_lenght=255)
+    picture = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True)
+    # parent = models.ManyToMany(Parent, related_name='parent_of_child') // FK u parenta?
+
+    def __str__(self):
+      return self.name
