a0a2c927954370c54260dd916adf9dc387761888
henryy07 <adam.harmasz@o2.pl>
2018-07-19 22:34:34 +0200

    changed models, delete Teacher import

--- kindergarten_project/accounts/models.py
+++ kindergarten_project/accounts/models.py
@@ -1,14 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-from groups.models import Child
+from groups.models import Child, Group
 
-User = settings.AUTH_USER_MODEL
+# User = settings.AUTH_USER_MODEL
 
 
 class Parent(models.Model):
-    first_name = models.CharField(max_lenght=255)
-    last_name = models.CharField(max_lenght)
-    child = models.ManyToMany(Child, related_name="childs_parent")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    child = models.ManyToManyField(Child, related_name="childs_parent")
     timestamp = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     child = models.ForeignKey(Child)
@@ -17,3 +19,4 @@ class Parent(models.Model):
 class Teacher(models.Model):
     user = models.OneToOneField(User)
     parent = models.ManyToManyField(Parent, related_name='guardian')
+    group = models.ManyToManyField(Group, related_name='group')
--- kindergarten_project/groups/models.py
+++ kindergarten_project/groups/models.py
@@ -1,21 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-from accounts.models import Teacher
+
+GROUP = (
+    ('Y', 'Younger'),
+    ('O','Older'),
+)
+
 
 class Child(models.Model):
-    name = models.CharField(max_lenght=255)
+    name = models.CharField(max_length=255)
     picture = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True)
     # parent = models.ManyToMany(Parent, related_name='parent_of_child') // FK u parenta?
 
     def __str__(self):
-      return self.name
+        return self.name
 
 
 class Group(models.Model):
     group_name = models.CharField(max_length=255)
     type_of_group = models.CharField(max_length=1, choices=GROUP)
     child = models.ForeignKey(Child)
-    teachers = models.ManyToManyField(Teacher, related_name='teacher_of_group') # trzeba zaimportować z accounts
+    # teachers = models.ManyToManyField(Teacher, related_name='teacher_of_group') # trzeba zaimportować z accounts
 
     def __str__(self):
         return self.group_name
