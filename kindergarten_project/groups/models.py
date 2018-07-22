57d581d419dc8cd97ccceb5bf6ea5663b7b4e3de
henryy07 <adam.harmasz@o2.pl>
2018-07-22 13:02:29 +0200

    context data

--- kindergarten_project/groups/models.py
+++ kindergarten_project/groups/models.py
@@ -1,35 +1,15 @@
 from django.conf import settings
 from django.db import models
 
-
-
 GROUP = (('Y', 'Younger'),
          ('O','Older'))
 
-<<<<<<< HEAD
-class Group(models.Model):
-    group_name = models.CharField(max_length=255)
-    type_of_group = models.CharField(max_length=1, choices=GROUP)
-    # child = models.ForeignKey(Child)
-    # teachers = models.ManyToManyField(Teacher, related_name='teacher_of_group') # trzeba zaimportować z accounts
-=======
-class Child(models.Model):
-    name = models.CharField(max_length=255)
-    #picture = models.ImageField(upload_to=settings.MEDIA_ROOT, blank=True)
-    # parent = models.ManyToMany(Parent, related_name='parent_of_child') // FK u parenta?
-
-    def __str__(self):
-        return self.name
-
 
 class Group(models.Model):
     group_name = models.CharField(max_length=255)
     type_of_group = models.CharField(max_length=1, choices=GROUP)
-    child = models.ForeignKey(Child)
-
-#	child = models.ForeignKey(Child)
+#	child = models.ForeignKey(Child) FK should be in child model so child will be assigned to the group
     #teachers = models.ManyToManyField(Teacher, related_name='teacher_of_group') // trzeba zaimportować z accounts
->>>>>>> 4c6d812c630ebc3efe17f9e4593d7c00e36c19ce
 
     def __str__(self):
         return self.group_name
